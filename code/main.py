1a8b59b9186f9b2d2f61bf07e92b8493f119b25c
lyricnz <lyricnz@users.noreply.github.com>
2023-07-12 23:54:23 +0000

    [MegaLinter] Apply linters fixes

--- code/main.py
+++ code/main.py
@@ -15,7 +15,11 @@ from data import Address, AddressList
 from db import AddressDB, add_db_arguments, connect_to_db
 from geojson import write_geojson_file
 from nbn import NBNApi
-from suburbs import read_all_suburbs, update_suburb_in_all_suburbs, update_processed_dates
+from suburbs import (
+    read_all_suburbs,
+    update_processed_dates,
+    update_suburb_in_all_suburbs,
+)
 
 
 def select_suburb(target_suburb: str, target_state: str) -> tuple[str, str]:
