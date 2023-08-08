535822cdf57b61f12196bf5dfe2503f40ccb023f
Simon Roberts <lyricnz@gmail.com>
2023-08-08 19:42:31 +1000

    Add simple test for select_suburb()

---
+++ code/test/test_select_suburb.py
@@ -0,0 +1,59 @@
+# test select_suburb method
+import pytest
+
+import main
+import utils
+
+
+def _dummy_read_json_file_combined_suburbs(filename: str) -> dict:
+    """Fake combined-suburbs.json file."""
+    assert filename == "results/combined-suburbs.json"
+    return {
+        "ACT": [
+            {
+                "address_count": 2596,
+                "announced": False,
+                "announced_date": "November 2024",
+                "name": "Ainslie",
+                "processed_date": "2022-08-02T17:44:18.007411",
+            },
+            {
+                "address_count": 312,
+                "announced": False,
+                "announced_date": None,
+                "name": "Acton",
+                "processed_date": None,
+            },
+            {
+                "address_count": 2372,
+                "announced": True,
+                "announced_date": None,
+                "name": "Amaroo",
+                "processed_date": None,
+            },
+            {
+                "address_count": 1074,
+                "announced": False,
+                "announced_date": None,
+                "name": "Aranda",
+                "processed_date": "2021-07-07T04:13:03.074294",
+            },
+        ]
+    }
+
+
+def test_select_unprocessed(monkeypatch):
+    """Test select_suburb method behaves in prescribed manner."""
+    monkeypatch.setattr(
+        utils,
+        "read_json_file",
+        lambda *args, **kwargs: _dummy_read_json_file_combined_suburbs(*args, **kwargs),
+    )
+
+    selector = main.select_suburb(None, None)
+    assert next(selector)[0] == "ACTON"  # unprocessed 1
+    assert next(selector)[0] == "AMAROO"  # unprocessed 2
+    assert next(selector)[0] == "ARANDA"  # old announced
+    assert next(selector)[0] == "AINSLIE"  # old unannounced
+    with pytest.raises(StopIteration):
+        next(selector)
--- code/test/test_utils.py
+++ code/test/test_utils.py
@@ -2,9 +2,11 @@ import pytest
 
 import utils
 
+
 def _read_file_string(filename: str) -> str:
     with open(filename) as f:
         return f.read()
+
 
 def test_write_json_file():
     test_data = {"a": 1, "b": 2, "c": {"d": 3, "e": 4}}
@@ -12,10 +14,12 @@ def test_write_json_file():
     read_data = utils.read_json_file("test.json")
     assert test_data == read_data
 
-@pytest.mark.skip(reason="waiting for https://github.com/LukePrior/nbn-upgrade-map/pull/177")
+
+@pytest.mark.skip(
+    reason="waiting for https://github.com/LukePrior/nbn-upgrade-map/pull/177"
+)
 def test_minimised_json():
     test_data = {"a": 1, "b": 2, "c": {"d": 3, "e": 4}}
     utils.write_json_file("test.json", test_data, indent=0)
     s = _read_file_string("test.json")
     assert s == '{"a":1,"b":2,"c":{"d":3,"e":4}}'
-
