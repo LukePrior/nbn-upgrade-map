b15460aa38ecd2acc13529412e1f7674c7afbf28
Simon Roberts <lyricnz@gmail.com>
2023-09-18 12:24:43 +1000

    Add integration test for NBN website parsing

---
+++ tests/test_adhoc_tools.py
@@ -0,0 +1,11 @@
+import adhoc_tools
+import data
+
+
+def test_get_nbn_suburb_dates():
+    """integration test"""
+    suburb_dates = adhoc_tools.get_nbn_suburb_dates()
+    assert len(suburb_dates) == len(data.STATES) - 1  # no "Other Territories"
+    for state, suburb_list in suburb_dates.items():
+        assert 'A-C' not in suburb_list
+        assert len(suburb_list) > 10
