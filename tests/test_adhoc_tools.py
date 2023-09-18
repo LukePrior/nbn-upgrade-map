0a9a4e22b863697ff1dc494d1c1f34740d6a5376
Simon Roberts <lyricnz@gmail.com>
2023-09-18 13:19:40 +1000

    Make reporting test more robust

--- tests/test_adhoc_tools.py
+++ tests/test_adhoc_tools.py
@@ -1,5 +1,8 @@
+import datetime
+
 import adhoc_tools
 import data
+import testutils
 
 
 def test_get_nbn_suburb_dates():
@@ -10,7 +13,16 @@ def test_get_nbn_suburb_dates():
         assert "A-C" not in suburb_list
         assert len(suburb_list) > 10
 
-def test_check_processing_rate():
+
+def test_check_processing_rate(monkeypatch):
+    """Check the reporting function"""
+    monkeypatch.setattr(
+        "utils.read_json_file",
+        lambda filename: testutils.read_test_data_json("combined-suburbs.json"),
+    )
+
     data = adhoc_tools.check_processing_rate()
-    assert len(data) > 10
-    assert data[-1][0] == "TOTAL"
+    assert len(data) == 3
+    assert data[0] == (datetime.date(2021, 7, 7), 1, None)
+    assert data[1] == (datetime.date(2022, 8, 2), None, 1)
+    assert data[-1] == ("TOTAL", 1, 1)
