8517f12a447db4d94f3713169351bc4e9dea6b8a
Simon Roberts <lyricnz@gmail.com>
2023-09-22 10:54:29 +1000

    Use SMALLINT for postcode

--- extra/db/create-table.sql
+++ extra/db/create-table.sql
@@ -3,7 +3,7 @@ CREATE TABLE gnaf_cutdown.address_principals
   gnaf_pid text NOT NULL,
   address text NOT NULL,
   locality_name text NOT NULL,
-  postcode text NULL,
+  postcode SMALLINT NULL,
   state text NOT NULL,
   latitude numeric(10,8) NOT NULL,
   longitude numeric(11,8) NOT NULL
