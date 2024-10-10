2a7a7496c11707b1b79ca0cec5b35896dd9d4e1e
LukePrior <LukePrior@users.noreply.github.com>
2024-10-10 15:51:40 +0000

    [MegaLinter] Apply linters fixes

--- code/sitemap.py
+++ code/sitemap.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+
 import utils
 
 
@@ -7,7 +8,7 @@ def generate_sitemap(json_file, output_file):
     data = utils.read_json_file(json_file)
 
     # Create the root element for the XML sitemap
-    urlset = ET.Element('urlset', xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1")
+    urlset = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap-image/1.1")
 
     # Add the static stats page without a lastmod tag
     add_url(urlset, "https://nbn.lukeprior.com/stats")
@@ -15,26 +16,26 @@ def generate_sitemap(json_file, output_file):
     # Generate URLs for each suburb
     for state, suburbs in data.items():
         for suburb in suburbs:
-            suburb_name = suburb['name']
-            processed_date = suburb['processed_date']
+            suburb_name = suburb["name"]
+            processed_date = suburb["processed_date"]
             url = f"https://nbn.lukeprior.com/?suburb={suburb_name.lower().replace(' ', '-')}&state={state.lower()}"
             add_url(urlset, url, processed_date)
 
     # Convert the XML tree to a string
-    sitemap_xml = ET.tostring(urlset, encoding='utf-8', xml_declaration=True).decode('utf-8')
+    sitemap_xml = ET.tostring(urlset, encoding="utf-8", xml_declaration=True).decode("utf-8")
 
     # Save the XML to a file
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         f.write(sitemap_xml)
 
 
 def add_url(urlset, loc, lastmod=None):
-    url = ET.SubElement(urlset, 'url')
-    loc_element = ET.SubElement(url, 'loc')
+    url = ET.SubElement(urlset, "url")
+    loc_element = ET.SubElement(url, "loc")
     loc_element.text = loc
     if lastmod:
-        lastmod_element = ET.SubElement(url, 'lastmod')
+        lastmod_element = ET.SubElement(url, "lastmod")
         lastmod_element.text = lastmod
 
 
-generate_sitemap('results/combined-suburbs.json', 'site/sitemap.xml')
+generate_sitemap("results/combined-suburbs.json", "site/sitemap.xml")
